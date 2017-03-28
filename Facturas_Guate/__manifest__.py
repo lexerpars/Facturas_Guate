d3d48bc1b5a8c9e41de7f9c1c745dbc07e281fd1
lexerpars <gersonrogelio2@gmail.com>
2017-03-28 15:23:00 -0600

    Formato Impresion Factura
    Formato de impresion de factura pre-impresa

---
+++ Facturas_Guate/__manifest__.py
@@ -0,0 +1,27 @@
+# _*_ coding: utf-8 _*_
+{
+	'name': "Reporte",
+
+	'summary': """
+		Reporte Facturas Mario Edyberto Hernandez Cardona""",
+
+	'description': """
+		Modulo para generar reporte de facturas para empresa 
+		MARIO EDYBERTO HERNANDEZ CARDONA, con un formato ya 
+		establecido
+	""",
+
+	'author': "Integratec",
+
+	'website': "http://www.integratec.com.gt",
+
+	'category': 'Uncategorized',
+
+	'version': '0.1',
+
+	'depends': ['base','account'],
+
+	'data': [
+		'report/report.xml',
+	]
+}
